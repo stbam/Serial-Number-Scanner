67708a5acfc8b3e83e6ac9e9f06950439935bf4d
stbam <55661516+stbam@users.noreply.github.com>
2024-07-30 19:31:36 -0400

    new boilerplate code

---
+++ app/src/components/CameraScreen.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView, StyleSheet} from 'react-native';
+import React from 'react'
+
+const CameraScreen = () => {
+  return (
+    <SafeAreaView style={{flex:1}}>
+          <View>
+             <Text>CameraScreen</Text>
+        </View>
+    </SafeAreaView>
+   
+  )
+  
+}
+
+export default CameraScreen
