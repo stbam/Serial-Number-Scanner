1ca9cd78c487b393ec45d2a34588080910a462cc
stbam <55661516+stbam@users.noreply.github.com>
2024-07-31 12:33:57 -0400

    camera can now be accessed on the android expo app

--- app/src/components/CameraScreen.tsx
+++ app/src/components/CameraScreen.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import { SafeAreaView, StyleSheet} from 'react-native';
+import {useCameraPermission,useCameraDevice,Camera} from 'react-native-vision-camera';
 import React from 'react'
+import { useEffect } from 'react';
+
 
 const CameraScreen = () => {
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const device =  useCameraDevice('back'); {/* back means we use camera from the back */}
+
+
+  useEffect(() =>{
+      if(!hasPermission){
+        requestPermission();
+
+      }
+  },[hasPermission])
+  if(!hasPermission){
+    return <ActivityIndicator/>
+  }
+
+  console.log(hasPermission);
+
+  if(!device){
+    return <Text>Camera device not found</Text>
+  }
   return (
     <SafeAreaView style={{flex:1}}>
-          <View>
-             <Text>CameraScreen</Text>
+          <View style={{flex:1}}>
+             <Camera style={StyleSheet.absoluteFill} device={device} isActive={true}></Camera>
         </View>
     </SafeAreaView>
    
